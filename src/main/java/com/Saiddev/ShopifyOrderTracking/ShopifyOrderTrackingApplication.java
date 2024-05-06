a1722a0b08c3c261184b0daf37af0e7109276cbe
saidsahinbas <saidsahinbas@gmail.com>
2024-05-06 14:07:45 +0300

    Spring app Initial

---
+++ src/main/java/com/Saiddev/ShopifyOrderTracking/ShopifyOrderTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.Saiddev.ShopifyOrderTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyOrderTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifyOrderTrackingApplication.class, args);
+	}
+
+}
